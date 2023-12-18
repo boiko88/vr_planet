63ff20cafd85ed418da696cb83d00c78165bd858
unknown <bo1ko.ant@yandex.com>
2023-12-18 16:26:02 +0300

    modified the text color and centered all pages names. Also, added background color for the footer that seems to be a little bit different from the navbar

--- social/urls.py
+++ social/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 
-from .views import HomeView, EquipmentView, GamesView, ForumView, HeadsetsView, UserRegistrationView, BlogView
+from .views import (
+    HomeView, 
+    EquipmentView, 
+    GamesView, 
+    ForumView, 
+    HeadsetsView, 
+    UserRegistrationView, 
+    BlogView)
 
 
 urlpatterns = [
@@ -13,7 +20,5 @@ urlpatterns = [
     path('blog', BlogView.as_view(), name="blog"),
     path('login/', auth_views.LoginView.as_view(template_name='login1.html', success_url='/'), name='login'),
     path('register/', UserRegistrationView.as_view(), name='user_register'),
-    # path('user_login/', LoginView.as_view(), name='user_login'),
-    # path('login1/', LoginView.as_view(), name='login1'),
     path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='user_logout'),
 ]
