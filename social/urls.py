fb2597b70782f22a5b6b02a4f9221f6532be1caf
unknown <bo1ko.ant@yandex.com>
2024-01-01 17:37:06 +0300

    added styling stat looks the same for the CRUD functionality of Blog and Games styling is also changed now

--- social/urls.py
+++ social/urls.py
@@ -18,12 +18,12 @@ from .views import (
 
 
 urlpatterns = [
-    path('', HomeView.as_view(), name="home"),
-    path('equipment', EquipmentView.as_view(), name="equipment"),
-    path('games', GamesView.as_view(), name="games"),
-    path('forum', ForumView.as_view(), name="forum"),
-    path('headsets', HeadsetsView.as_view(), name="headsets"),
-    path('blog', BlogView.as_view(), name="blog"),
+    path('', HomeView.as_view(), name='home'),
+    path('equipment', EquipmentView.as_view(), name='equipment'),
+    path('games', GamesView.as_view(), name='games'),
+    path('forum', ForumView.as_view(), name='forum'),
+    path('headsets', HeadsetsView.as_view(), name='headsets'),
+    path('blog', BlogView.as_view(), name='blog'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html', success_url='/'), name='login'),
     path('register/', UserRegistrationView.as_view(), name='register'),
     path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='user_logout'),
